ea0e1a543bf2c8e502dd751108d198debea83222
ssbrandt <sierrasbrandt@gmail.com>
2023-08-23 18:53:34 -0400

    address popupwithimage comments

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,12 +3,14 @@ import Popup from "../components/Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._popupElement = document.querySelector(popupSelector);
+    // this._popupElement = document.querySelector(popupSelector);
+    this._popupImage = this._popupElement.querySelector(".modal__image");
+    this._popupTitle = this._popupElement.querySelector(".modal__title_image");
   }
 
   open({ image, title }) {
-    this._popupImage = this._popupElement.querySelector(".modal__image");
-    this._popupTitle = this._popupElement.querySelector(".modal__title_image");
+    // this._popupImage = this._popupElement.querySelector(".modal__image");
+    // this._popupTitle = this._popupElement.querySelector(".modal__title_image");
     this._popupImage.src = image;
     this._popupImage.alt = `Photo of ${title}`;
     this._popupTitle.textContent = title;
