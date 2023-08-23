bc9c5c7a7f9a98dd8fe1b724ea59f34877414ac9
ssbrandt <sierrasbrandt@gmail.com>
2023-08-22 20:02:05 -0400

    move to webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import Card from "/components/Card.js";
-import FormValidator from "/components/FormValidator.js";
-import UserInfo from "/components/UserInfo.js";
-import PopupWithImage from "/components/PopupWithImage.js";
-import PopupWithForm from "/components/PopupWithForm.js";
-import Section from "/components/Section.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import UserInfo from "../components/UserInfo.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import Section from "../components/Section.js";
 import { initialCards, config } from "../utils/constants.js";
+import "./index.css";
 
 //form validations
 
